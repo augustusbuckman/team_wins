e1b25b018885f4eca03a7615d501cf673d0ddb9c
Augustus Buckman <b020112032@live.gtuc.edu.gh>
2023-09-15 10:22:30 +0000

    Initial

---
+++ team_wins.py
@@ -0,0 +1,26 @@
+# File: team_wins.py
+# Description: <A DESCRIPTION OF YOUR PROGRAM>
+# Assignment Number: 6
+#
+# Name: <YOUR NAME>
+# SID:  <YOUR EID>
+# Email: <YOUR EMAIL>
+# Grader: <YOUR GRADER'S NAME Carolyn OR Emma OR Ahmad>
+# Slip days used this assignment: <#>
+#
+# On my honor, <YOUR NAME>, this programming assignment is my own work
+# and I have not provided this code to any other student. .
+
+
+#Main Function to call the other functions
+def main():
+
+
+
+#Create other functions
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ team_wins_test.py
@@ -0,0 +1,25 @@
+import os.path
+import sys
+from unittest import TestCase
+
+from zeller import main
+from tud_test_base import *
+
+def test_team_wins():
+    try:
+        exist = os.path.exists("team_wins.py")
+        assert exist == True
+    except:
+        sys.exit()
+
+    set_keyboard_input(['cfb05_s_e.txt', 'C Missouri'])
+    main()
+    output = get_display_output()
+    assert output == [
+        "This program reads a file for a sport's teams results.",
+        "Enter file name: ",
+        "Enter team name: ",
+        "wins: 1",
+        "losses: 0",
+        "win percentage: 100.0"
+    ]
